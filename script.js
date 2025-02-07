a7e331e306e4e137ea273e8e96605d8eceee0d81
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2025-02-06 22:43:35 -0300

    feat: finished js and html

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
-function adicionar(tarefa){
+function adicionar() {
     let tarefa = document.getElementById('tarefa').value;
-    let datahHora = document.getElementById('data-hora').value;
-    let prioridade = document.getElementById('prioridade').value;
+    let dataHora = document.getElementById('dataHora').value;
+    let diaSemana = document.getElementById('diaSemana').value;
+    let prioridade = document.querySelector('input[name="prioridade"]:checked').value;
 
-    let mensagem = 'Tarefa: ' + tarefa + '\nData e Hora: ' + datahHora + '\nPrioridade: ' + prioridade;
+    let mensagem = `Tarefa: ${tarefa} | Data e Hora: ${dataHora} | Dia da Semana: ${diaSemana} | Prioridade: ${prioridade.value}`;
 
-    document.getElementById("resposta").innerText = mensagem;
+    let novaTarefa = document.createElement('div');
+    novaTarefa.innerText = mensagem;
+
+    document.getElementById('tarefas').appendChild(novaTarefa);
+
+    let concluir = document.createElement('input');
+    concluir.type = 'checkbox';
+    concluir.onclick = function() {
+        concluirTarefa(novaTarefa, concluir);
+    };
+
+    let remover = document.createElement('button');
+    remover.innerText = 'Remover';
+    remover.onclick = function() {
+        removerTarefa(novaTarefa);
+    };
+
+    novaTarefa.appendChild(concluir);
+    novaTarefa.appendChild(remover);
+
+    document.getElementById("tarefas").appendChild(novaTarefa);
+}
+
+function removerTarefa(tarefa) {
+    tarefa.remove();
+}
+
+function concluirTarefa(tarefa, checkbox) {
+    if (checkbox.checked) {
+        tarefa.style.textDecoration = 'line-through';
+        document.getElementById('tarefas-concluidas').appendChild(tarefa);
+    } else {
+        tarefa.style.textDecoration = 'none';
+        document.getElementById('tarefas').appendChild(tarefa);
+    }
 }
