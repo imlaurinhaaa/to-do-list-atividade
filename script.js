6b645ae928f98db0db7f718bd9890524b37da32b
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2025-02-13 09:30:23 -0300

    style: style the list

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
 function adicionar() {
-    let tarefa = document.getElementById('tarefa').value;
-    let dataHora = document.getElementById('dataHora').value;
-    let diaSemana = document.getElementById('diaSemana').value;
+    let tarefa = document.getElementById("tarefa").value;
+    let dataHora = document.getElementById("dataHora").value;
+    let diaSemana = document.getElementById("diaSemana").value;
     let prioridade = document.querySelector('input[name="prioridade"]:checked').value;
 
-    let mensagem = `Tarefa: ${tarefa} | Data e Hora: ${dataHora} | Dia da Semana: ${diaSemana} | Prioridade: ${prioridade.value}`;
+    let mensagem = `📑 Tarefa: ${tarefa} 
+    ⌚ Data e Hora: ${dataHora} 
+    📅 Dia da Semana: ${diaSemana} 
+    📍 Prioridade: ${prioridade}`;
 
-    let novaTarefa = document.createElement('div');
+    let novaTarefa = document.createElement("div");
+    novaTarefa.className = "novaTarefa";
     novaTarefa.innerText = mensagem;
 
-    document.getElementById('tarefas').appendChild(novaTarefa);
+    document.getElementById("tarefas").appendChild(novaTarefa);
 
-    let concluir = document.createElement('input');
-    concluir.type = 'checkbox';
-    concluir.onclick = function() {
-        concluirTarefa(novaTarefa, concluir);
-    };
-
-    let remover = document.createElement('button');
-    remover.innerText = 'Remover';
-    remover.onclick = function() {
+    let remover = document.createElement("button");
+    remover.className = "remover";
+    remover.innerText = "Remover";
+    remover.onclick = function () {
         removerTarefa(novaTarefa);
     };
 
-    novaTarefa.appendChild(concluir);
     novaTarefa.appendChild(remover);
-
-    document.getElementById("tarefas").appendChild(novaTarefa);
 }
 
 function removerTarefa(tarefa) {
     tarefa.remove();
-}
-
-function concluirTarefa(tarefa, checkbox) {
-    if (checkbox.checked) {
-        tarefa.style.textDecoration = 'line-through';
-        document.getElementById('tarefas').appendChild(tarefa);
-    }
 }
